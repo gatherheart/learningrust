02613bf398f9be054465cf347e4859eca17efbce
gatherheart <gatherheart@naver.com>
2026-04-29 23:29:48 +0900

    Expand Rust lesson explanations and hide predict answers

--- lessons/src/bin/18_traits_derive.rs
+++ lessons/src/bin/18_traits_derive.rs
@@ -1,12 +1,14 @@
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
 }
 
 fn main() {
-    let p = Point { x: 1, y: 2 };
-    let q = p.clone();
-    println!("{:?}", p);
-    println!("equal={}", p == q);
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = p1;
+
+    println!("p1 = {}, {}", p1.x, p1.y);
+    println!("p2 = {}, {}", p2.x, p2.y);
+    println!("equal={}", p1 == p2);
 }
